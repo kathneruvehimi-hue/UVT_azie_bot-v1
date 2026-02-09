8d2f70c274e12f91fcca5b3913c25375fedccf67
kathneruvehimi-hue <kathneruvehimi@gmail.com>
2026-02-09 18:57:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 
 const client = new Client({
     authStrategy: new LocalAuth(),
-    authTimeoutMs: 0, 
+    authTimeoutMs: 0,
     puppeteer: {
         headless: true,
         executablePath: '/usr/bin/google-chrome-stable',
@@ -11,13 +11,13 @@ const client = new Client({
             '--disable-setuid-sandbox',
             '--disable-dev-shm-usage',
             '--no-zygote',
-            '--single-process',
-            '--disable-gpu'
+            '--disable-gpu',
+            '--disable-software-rasterizer',
+            '--single-process'
         ],
     }
 });
 
-// Pairing code එක ලැබෙන තැන
 client.on('pairing_code', (code) => {
     console.log('\n\n=========================================');
     console.log('ඔබේ Pairing Code එක මෙන්න: ' + code);
@@ -28,8 +28,10 @@ client.on('ready', () => {
     console.log('බොට් සාර්ථකව සම්බන්ධ වුණා!');
 });
 
-// සර්වර් එකට හුස්මක් ගන්න තත්පර 10ක ප්‍රමාදයක් ලබා දීම
+// සර්වර් එකට හුස්මක් ගන්න තත්පර 20ක ප්‍රමාදයක් ලබා දීම
+console.log("සර්වර් එක සූදානම් වන තෙක් තත්පර 20ක් රැඳී සිටින්න...");
 setTimeout(() => {
-    console.log("බොට් පණගන්වමින්... කරුණාකර රැඳී සිටින්න.");
-    client.initialize();
-}, 10000);
+    console.log("බොට් පණගන්වමින්...");
+    client.initialize().catch(err => console.log("Initialize Error: ", err.message));
+}, 20000);
+
